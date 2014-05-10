08631d8ade7a1c0314f792333fa2c8a4662da1f7
Jayaraj Poroor <jayaraj.poroor@gmail.com>
2014-05-10 23:17:14 +0530

    initial dev

--- src/grammar/testrun2.bat
+++
@@ -1,4 +0,0 @@
-call antlr4 ShelloidLexer.g4
-call antlr4 ShelloidParser.g4
-javac *.java
-call grun Shelloid script ..\..\test\grammar\sample-1.shd
--- src/script-engine/src/com/shelloid/script/Compiler.java
+++ src/script-engine/src/com/shelloid/script/Compiler.java
@@ -7,16 +7,55 @@
 package com.shelloid.script;
 import com.shelloid.script.parser.ShelloidLexer;
 import com.shelloid.script.parser.ShelloidParser;
+import com.shelloid.script.parser.ShelloidParser.ScriptContext;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.Token;
 /**
  *
  * @author Jayaraj Poroor
  */
 public class Compiler {
-    public void compile(InputStream is) throws IOException
+    ArrayList<String> errorMsgs = new ArrayList();
+    public void compile(InputStream is) throws IOException, CompilerException
     {
         ShelloidLexer lexer = new ShelloidLexer(new ANTLRInputStream(is));
+        ShelloidParser parser = new ShelloidParser(new CommonTokenStream(lexer));
+        parser.removeErrorListeners();
+        parser.addErrorListener(new CustomErrorListener());
+        ScriptContext script = parser.script();
+        if(errorMsgs.isEmpty())
+        {
+            return translateScript(script);
+        }else
+        {
+            throw new CompilerException(errorMsgs);
+        }
+    }
+    
+    void translateScript(ScriptContext script)
+    {
+        
+    }
+    
+    class CustomErrorListener extends BaseErrorListener
+    {
+        public void syntaxError(Recognizer<?,?> recognizer, Object offendingSymbol, 
+                                int line, int charPositionInLine, String msg, 
+                                RecognitionException e)
+        {
+            String tokenText = (offendingSymbol instanceof Token) ? 
+                               ((Token) offendingSymbol).getText() : "<unknown token>";
+            String errorMsg = 
+                           "Syntax Error at: " + line + ": " + charPositionInLine+
+                           " near " + tokenText;
+            errorMsgs.add(errorMsg);
+        }
     }
 }
